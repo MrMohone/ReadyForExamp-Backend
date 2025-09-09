36ced34938fc6bc49c2564afce4e600ff0e88025
Mohammed Awol <mohammedawol0837>
2025-09-10 01:55:37 +0300

    serializer file

---
+++ ready_exam/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import ExamType, Subject, Resource
+
+class ExamTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamType
+        fields = ['id', 'exam_name']
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    # Display exam_type as dropdown in browsable API
+    exam_type = serializers.PrimaryKeyRelatedField(
+        queryset=ExamType.objects.all()
+    )
+    exam_type_name = serializers.StringRelatedField(source='exam_type', read_only=True)  # optional, shows name
+
+    class Meta:
+        model = Subject
+        fields = ['id', 'subject_name', 'category', 'exam_type', 'exam_type_name']
+
+
+class ResourceSerializer(serializers.ModelSerializer):
+    # Display subject_name as dropdown in browsable API
+    subject_name = serializers.PrimaryKeyRelatedField(
+        queryset=Subject.objects.all()
+    )
+    subject_title_display = serializers.StringRelatedField(source='subject_name', read_only=True)  # optional
+
+    class Meta:
+        model = Resource
+        fields = ['id', 'subject_name', 'subject_title', 'resource_type', 'file', 'description', 'uploaded_at', 'subject_title_display']
--- ready_exam/views.py
+++ ready_exam/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import ExamType, Subject, Resource
+from .serializers import ExamTypeSerializer, SubjectSerializer, ResourceSerializer
 
-def index(request):
-    return HttpResponse("Hello, Exam Ready App is working 🚀")
+class ExamTypeViewSet(viewsets.ModelViewSet):
+    queryset = ExamType.objects.all()
+    serializer_class = ExamTypeSerializer
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
