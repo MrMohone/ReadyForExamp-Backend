d9a265dfc590f86f298566d2bb6fae118868ccc8
Mohammed Awol <mohammedawol0837>
2025-09-10 22:17:53 +0300

    backend corsheaders setup filled

--- main/settings.py
+++ main/settings.py
@@ -29,17 +29,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ready_exam',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware", # CORS middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",       # Alternative local host
+    "https://your-frontend.com",   # production frontend domain
+]
+
 
 ROOT_URLCONF = 'main.urls'
  
